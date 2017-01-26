740adb028bdf1cae698b7685852f98202c13bb6f
Richard <richard.turza@gmail.com>
2017-01-26 12:44:30 +0000

    finished, probably

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 "use strict";
+
+var express = require("express");
+var multer = require("multer");
+var fs = require("fs");
+
+var upload = multer({ dest: 'uploads/' });
+var port = process.env.PORT;
+
+express()
+.use(express.static(__dirname+"/html"))
+.get("/", (req,res) => {
+    res.sendFile("index.html");
+})
+.post("/get-file-size", upload.single('fileWithSize'), (req,res) => {
+      console.dir(req.file);
+      
+      if (req.file) {
+          
+          fs.unlink(req.file.path, () => {
+              res.end(JSON.stringify({size:req.file.size}));
+          })
+          
+      } else {
+          res.end("No file uploaded");
+      }
+})
+.listen(port || 8080);
